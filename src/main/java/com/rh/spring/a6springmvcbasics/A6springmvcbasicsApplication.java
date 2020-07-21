ff1955983c380af7424cbca9d26af5ed6e35586f
RovshanHuseynov <rovsen.huseyn.oglu@gmail.com>
2020-07-21 11:16:32 +0400

    Initial commit

---
+++ src/main/java/com/rh/spring/a6springmvcbasics/A6springmvcbasicsApplication.java
@@ -0,0 +1,13 @@
+package com.rh.spring.a6springmvcbasics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A6springmvcbasicsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(A6springmvcbasicsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rh/spring/a6springmvcbasics/A6springmvcbasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rh.spring.a6springmvcbasics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class A6springmvcbasicsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
